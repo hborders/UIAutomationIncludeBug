255986c9bcb7aaa81d7a48ccd07f3d357ab636ff
Heath Borders <heath.borders@asolutions.com>
2012-10-24 14:53:57 -0500

    Initial Commit

---
+++ UIAutomationIncludeBug/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  UIAutomationIncludeBug
+//
+//  Created by dev1 on 10/24/12.
+//  Copyright (c) 2012 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
